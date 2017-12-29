1274073fb109b1e4a988e349331dd8a8b1d2c03c
JP Dillingham <jpdillingham@gmail.com>
2017-12-29 15:32:38 -0600

    Added quick Cognito example

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { CognitoUserPool, CognitoUserAttribute, CognitoUser } from 'amazon-cognito-identity-js'
 
 import App from './components/App'
 
