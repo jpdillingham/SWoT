54663a81a789d1b10b483a7c44d078f3c834f584
JP Dillingham <jpdillingham@gmail.com>
2017-12-30 17:27:18 -0600

    Added custom theme

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,52 +1,35 @@
 import React, { Component } from 'react';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AppBar from 'material-ui/AppBar'
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+import AppBar from 'material-ui/AppBar';
+
 import Drawer from 'material-ui/Drawer'
 import MenuItem from 'material-ui/MenuItem'
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
-import { CognitoUserPool, CognitoUserAttribute, CognitoUser } from 'amazon-cognito-identity-js'
 
 class App extends Component {
     state = {
         drawerOpen: false,
     }
 
+    theme = getMuiTheme({
+        palette: {
+            primary1Color: "#2196f3",
+            "primary2Color": "#0288d1",
+            "pickerHeaderColor": "#29b6f6"
+        }
+    })
+
     toggleDrawer = () => {
         this.setState({ drawerOpen: !this.state.drawerOpen })
     }
 
-    signup = () => {
-        let poolData = {
-            UserPoolId: 'us-east-1_OhnX3yEY5',
-            ClientId: '18b132av2gkfgl3m793qcgjsd3'
-        }
-    
-        let userPool = new CognitoUserPool(poolData);
-        let attributeList = [];
-        let dataEmail = {
-            Name: 'test',
-            Value: 'test@whatnet.us'
-        }
-    
-        let attributeEmail = new CognitoUserAttribute(dataEmail);
-
-        //attributeList.push(attributeEmail);
-
-        userPool.signUp('cognitotest@whatnet.us', 'Password1@', attributeList, null, function(err, result) {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                console.log(result.user);
-            }
-        })
-    }
-
     render() {
         return (
-            <MuiThemeProvider>
+            <MuiThemeProvider muiTheme={this.theme}>
                 <AppBar 
                     title="SWoT" 
                     style={styles.appBar}
@@ -54,18 +37,19 @@ class App extends Component {
                 />
                 <Drawer 
                     open={this.state.drawerOpen}
-                    docked={true}
-                    containerStyle={{height: 'calc(100% - 64px)', top: 64}}
+                    docked={false}
+                    onRequestChange={this.toggleDrawer}
                 >
+                                <AppBar 
+                    title="SWoT" showMenuIconButton={false}
+                />
                     <MenuItem>Menu Item</MenuItem>
                     <MenuItem>Menu Item 2</MenuItem>
                 </Drawer>
-                <div style={{marginLeft: this.state.drawerOpen ? 255 : 0}}>
+                <div style={{marginLeft: this.state.drawerOpen ? 0 : 0}}>
                     <h1>Hello World!</h1>
                     <FlatButton label="Primary" primary={true} />
                     <RaisedButton label="Primary" primary={true} />
-                    {/*<FlatButton onClick={() => this.signup()}>Sign up!</FlatButton>
-                    <RaisedButton label="Primary">Raised!</RaisedButton>*/}
                 </div>
             </MuiThemeProvider>
         );
