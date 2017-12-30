f72a77d8e94a1183959486e975684fd89f92cf11
JP Dillingham <jpdillingham@gmail.com>
2017-12-30 13:44:12 -0600

    Began scaffolding UI

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar'
+import Drawer from 'material-ui/Drawer'
+import MenuItem from 'material-ui/MenuItem'
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 import { CognitoUserPool, CognitoUserAttribute, CognitoUser } from 'amazon-cognito-identity-js'
 
 class App extends Component {
+    state = {
+        drawerOpen: false,
+    }
+
+    toggleDrawer = () => {
+        this.setState({ drawerOpen: !this.state.drawerOpen })
+    }
+
     signup = () => {
         let poolData = {
             UserPoolId: 'us-east-1_OhnX3yEY5',
@@ -31,11 +46,37 @@ class App extends Component {
 
     render() {
         return (
-        <div>
-            <h1>Hello World!</h1>
-            <button onClick={() => this.signup()}>Sign up!</button>
-        </div>
+            <MuiThemeProvider>
+                <AppBar 
+                    title="SWoT" 
+                    style={styles.appBar}
+                    onLeftIconButtonClick={this.toggleDrawer}
+                />
+                <Drawer 
+                    open={this.state.drawerOpen}
+                    docked={true}
+                    containerStyle={{height: 'calc(100% - 64px)', top: 64}}
+                >
+                    <MenuItem>Menu Item</MenuItem>
+                    <MenuItem>Menu Item 2</MenuItem>
+                </Drawer>
+                <div style={{marginLeft: this.state.drawerOpen ? 255 : 0}}>
+                    <h1>Hello World!</h1>
+                    <FlatButton label="Primary" primary={true} />
+                    <RaisedButton label="Primary" primary={true} />
+                    {/*<FlatButton onClick={() => this.signup()}>Sign up!</FlatButton>
+                    <RaisedButton label="Primary">Raised!</RaisedButton>*/}
+                </div>
+            </MuiThemeProvider>
         );
+    }
+}
+
+const styles = {
+    appBar: {
+        position: 'fixed',
+        left: 0,
+        top: 0,
     }
 }
 
