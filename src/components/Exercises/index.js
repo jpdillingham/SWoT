e6434f605b22d96cb46d376e56416fb4dda6c07b
JP Dillingham <jpdillingham@gmail.com>
2018-01-01 14:58:58 -0600

    Refactored Exercises and Routines to use constants.js

--- src/components/Exercises/index.js
+++ src/components/Exercises/index.js
@@ -9,72 +9,19 @@ import Paper from 'material-ui/Paper';
 
 import ExercizeCard from './ExerciseCard'
 
+import { EXERCISES } from '../../constants'
+
 class Exercises extends Component {
     render() {
-        let exercises = [
-            { 
-                id: 'c4a10b9d-d5de-434e-bac4-c3a4ff014f82',
-                name: 'bench press',
-                type: 'weightlifting',
-                url: 'https://www.bodybuilding.com/exercises/barbell-bench-press-medium-grip',
-                metrics: [
-                    {
-                        name: 'weight',
-                        uom: 'lbs',
-                    },
-                    {
-                        name: 'sets',
-                    },
-                    {
-                        name: 'reps',
-                    }
-                ]
-            },
-            { 
-                id: '29d53257-5eca-4083-9019-81dc62425801',
-                name: 'squat',
-                type: 'weightlifting',
-                url: 'https://www.bodybuilding.com/exercises/barbell-full-squat',
-                metrics: [
-                    {
-                        name: 'weight',
-                        uom: 'lbs',
-                    },
-                    {
-                        name: 'sets',
-                    },
-                    {
-                        name: 'reps',
-                    }
-                ] 
-            },
-            { 
-                id: '0f2f3a76-c1a2-4a53-bec9-0f124a1f3b16',
-                name: 'running',
-                type: 'cardio',
-                url: 'https://www.bodybuilding.com/exercises/running-treadmill',
-                metrics: [
-                    {
-                        name: 'distance',
-                        uom: 'miles',
-                    },
-                    {
-                        name: 'time',
-                        uom: 'minutes',
-                    }
-                ]
-            },
-        ]
-
         return (
             <div>
                 <FloatingActionButton secondary={true} zDepth={4} style={styles.fab}>
                     <ContentAdd />
                 </FloatingActionButton>
                 <div style={styles.grid}>
-                {exercises.map(e =>  
+                {EXERCISES.map(e =>  
                     <div>
-                        <ExercizeCard title={e.name} subtitle={e.url} metrics={e.metrics}>
+                        <ExercizeCard exercise={e}>
                         </ExercizeCard>
                     </div>
                 )}
