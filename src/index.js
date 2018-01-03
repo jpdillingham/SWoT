f0a901569395d64a61c1e40aafdfbab9a5b0f032
JP Dillingham <jpdillingham@gmail.com>
2018-01-02 20:54:24 -0600

    Added redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import { EXERCISES, ROUTINES } from './constants'
 
 import App from './components/App'
 
+const initialState = {
+    exercises: EXERCISES,
+    routines: ROUTINES
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'ADD_EXERCISE':
+            return  Object.assign({}, state, { 
+                exercises: state.exercises.concat(action.exercise) 
+            });
+        case 'DELETE_EXERCISE':
+            return Object.assign({}, state, {
+                exercises: state.exercises.filter(e => e.id != action.id)
+            });
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+
 ReactDOM.render(
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>, 
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>, 
     document.getElementById('root')
 );
 
