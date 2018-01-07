e5e1f822c6934ad4ffe00fc295c40adb166cc3bb
JP Dillingham <jpdillingham@gmail.com>
2018-01-07 16:38:54 -0600

    Changed capitalization in constants

--- src/constants.js
+++ src/constants.js
@@ -1,71 +1,71 @@
-export const EXERCISE_TYPES = [ 'weightlifting', 'bodyweight', 'cardio', 'stretching', 'balance' ]
+export const EXERCISE_TYPES = [ 'Weightlifting', 'Bodyweight', 'Cardio', 'Stretching', 'Balance' ]
 
 export const EXERCISE_URL_BASE = 'https://www.bodybuilding.com/exercises/'
 
 export const EXERCISES = [
     { 
         id: 'c4a10b9d-d5de-434e-bac4-c3a4ff014f82',
-        name: 'bench press',
-        type: 'weightlifting',
+        name: 'Bench Press',
+        type: 'Weightlifting',
         url: 'https://www.bodybuilding.com/exercises/barbell-bench-press-medium-grip',
         metrics: [
             {
-                name: 'weight',
-                uom: 'lbs',
+                name: 'Weight',
+                uom: 'Lbs',
             },
             {
-                name: 'sets',
+                name: 'Sets',
             },
             {
-                name: 'reps',
+                name: 'Reps',
             }
         ]
     },
     { 
         id: '29d53257-5eca-4083-9019-81dc62425801',
-        name: 'squat',
-        type: 'weightlifting',
+        name: 'Squat',
+        type: 'Weightlifting',
         url: 'https://www.bodybuilding.com/exercises/barbell-full-squat',
         metrics: [
             {
-                name: 'weight',
-                uom: 'lbs',
+                name: 'Weight',
+                uom: 'Lbs',
             },
             {
-                name: 'sets',
+                name: 'Sets',
             },
             {
-                name: 'reps',
+                name: 'Reps',
             }
         ] 
     },
     { 
         id: '0f2f3a76-c1a2-4a53-bec9-0f124a1f3b15',
-        name: 'chinups',
-        type: 'bodyweight',
+        name: 'Chinups',
+        type: 'Bodyweight',
         url: 'https://www.bodybuilding.com/exercises/chin-up',
         metrics: [
             {
-                name: 'sets',
+                name: 'Sets',
             },
             {
-                name: 'reps',
+                name: 'Reps',
             }
         ]
     },
     { 
         id: '0f2f3a76-c1a2-4a53-bec9-0f124a1f3b16',
-        name: 'running',
-        type: 'cardio',
+        name: 'Running',
+        type: 'Cardio',
         url: 'https://www.bodybuilding.com/exercises/running-treadmill',
         metrics: [
             {
-                name: 'distance',
-                uom: 'miles',
+                name: 'Distance',
+                uom: 'Miles',
             },
             {
-                name: 'time',
-                uom: 'minutes',
+                name: 'Time',
+                uom: 'Minutes',
             }
         ]
     },
@@ -74,14 +74,14 @@ export const EXERCISES = [
 export const ROUTINES = [
     {
         id: 'f5d161a9-4913-4052-bc80-c82643ba7d25',
-        name: 'cardio',
+        name: 'Cardio',
         exercises: [
             { id: '0f2f3a76-c1a2-4a53-bec9-0f124a1f3b16' }, // 'running'
         ]
     },
     {
         id: 'f8a39189-0824-4dd6-a621-eb76ea85306d',
-        name: 'workout A',
+        name: 'Workout A',
         exercises: [
             { id: 'c4a10b9d-d5de-434e-bac4-c3a4ff014f82' }, // 'bench press'
             { id: '29d53257-5eca-4083-9019-81dc62425801' }, // 'squat'
