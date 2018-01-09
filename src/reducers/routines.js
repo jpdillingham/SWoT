03fe15761cebbb70188a7459c745fcdcf3c71537
JP Dillingham <jpdillingham@gmail.com>
2018-01-08 22:31:53 -0600

    Added routines reducer

---
+++ src/reducers/routines.js
@@ -0,0 +1,8 @@
+import { ROUTINES } from '../constants'
+
+export default function routines(state = ROUTINES, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
