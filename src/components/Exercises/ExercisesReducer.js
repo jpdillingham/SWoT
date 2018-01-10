7353a167f1cfacaf84250d02780e38138b9be247
JP Dillingham <jpdillingham@gmail.com>
2018-01-09 21:46:44 -0600

    Moved Exercises reducer to Exercises folder

---
+++ src/components/Exercises/ExercisesReducer.js
@@ -0,0 +1,20 @@
+const initialState = []
+
+const ExercisesReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'SET_EXERCISES':
+            return action.exercises;
+        case 'ADD_EXERCISE':
+            return state.concat(action.exercise);
+        case 'DELETE_EXERCISE':
+            return state.filter(e => e.id !== action.id);
+        case 'UPDATE_EXERCISE':
+            return state.map(e => { 
+                    return e.id === action.exercise.id ? action.exercise : e
+                })
+        default:
+            return state;
+    }
+}
+
+export default ExercisesReducer
--- src/reducers.js
+++ src/reducers.js
@@ -1,29 +1,12 @@
 import { combineReducers } from 'redux'
 import { ROUTINES } from './constants'
+import ExercisesReducer from './components/exercises/ExercisesReducer'
 
 const initialState = {
-    exercises: [],
     routines: ROUTINES,
     snackbar: {
         visible: false,
         message: ''
-    }
-}
-
-const exercises = (state = initialState.exercises, action) => {
-    switch (action.type) {
-        case 'SET_EXERCISES':
-            return action.exercises;
-        case 'ADD_EXERCISE':
-            return state.concat(action.exercise);
-        case 'DELETE_EXERCISE':
-            return state.filter(e => e.id !== action.id);
-        case 'UPDATE_EXERCISE':
-            return state.map(e => { 
-                    return e.id === action.exercise.id ? action.exercise : e
-                })
-        default:
-            return state;
     }
 }
 
@@ -46,8 +29,8 @@ const snackbar = (state = initialState.snackbar, action) => {
 }
 
 const rootReducer = combineReducers({ 
-    exercises, 
     snackbar, 
+    exercises: ExercisesReducer,
     routines 
 })
   
