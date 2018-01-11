907f7074d061dd05c3bbfb6b92a1cee5edb50b01
JP Dillingham <jpdillingham@gmail.com>
2018-01-10 22:05:33 -0600

    Added request/receive reducers to Exercises

--- src/components/Exercises/ExercisesReducer.js
+++ src/components/Exercises/ExercisesReducer.js
@@ -1,9 +1,26 @@
-const initialState = []
+const initialState = {
+    isFetching: false,
+    isErrored: false,
+    items: []
+}
 
 const ExercisesReducer = (state = initialState, action) => {
+    console.log(action)
     switch (action.type) {
-        case 'SET_EXERCISES':
-            return action.exercises;
+        case 'REQUEST_EXERCISES':
+            return { 
+                ...state, 
+                isFetching: true, 
+                isErrored: false,
+                items: []
+            };
+        case 'RECEIVE_EXERCISES':
+            return { 
+                ...state, 
+                isFetching: false, 
+                isErrored: action.status === 200 ? false : true,
+                items: action.items
+             }
         case 'ADD_EXERCISE':
             return state.concat(action.exercise);
         case 'DELETE_EXERCISE':
