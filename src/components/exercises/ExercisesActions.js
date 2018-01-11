9acfee857a8c1148a21fc1932e70ec5d5952629d
JP Dillingham <jpdillingham@gmail.com>
2018-01-10 22:05:44 -0600

    Added ExercisesActions

---
+++ src/components/exercises/ExercisesActions.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+
+export const REQUEST_EXERCISES = 'REQUEST_EXERCISES'
+
+function requestExercises() {
+    return {
+        type: REQUEST_EXERCISES
+    }
+}
+
+export const RECEIVE_EXERCISES = 'RECEIVE_EXERCISES'
+
+function receiveExercises(status, items) {
+    return {
+        type: RECEIVE_EXERCISES,
+        status: status,
+        items: items
+    }
+}
+
+const endpoint = 'https://16xkdlfrol.execute-api.us-east-1.amazonaws.com/deployment'
+
+export function fetchExercises() {
+    console.log('thunk...')
+    return function(dispatch, getState) {
+        dispatch(requestExercises())
+
+        axios.get(endpoint)
+            .then(response => { 
+                dispatch(receiveExercises(response.status, response.data))
+            })      
+    }
+}
