d3731b8467490e00869ddd9218b89c3706bde288
JP Dillingham <jpdillingham@gmail.com>
2018-01-11 21:10:34 -0600

    Simplified snackbar show reducer

--- src/components/app/AppReducer.js
+++ src/components/app/AppReducer.js
@@ -8,7 +8,7 @@ const initialState = {
 const AppReducer = (state = initialState, action) => { 
     switch (action.type) {
         case 'SNACKBAR_SHOW':
-            return { ...state, snackbar: action.snackbar };
+            return { ...state, snackbar: { visible: true, message: action.message } };
         case 'SNACKBAR_HIDE':
             return { ...state, snackbar: { visible: false, message: '' } }
         default:
