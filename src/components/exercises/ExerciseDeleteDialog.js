8d1f2a2e42c5809a9d1d3ba7e07ed928112f407a
JP Dillingham <jpdillingham@gmail.com>
2018-01-15 20:05:38 -0600

    Implemented delete api

--- src/components/exercises/ExerciseDeleteDialog.js
+++ src/components/exercises/ExerciseDeleteDialog.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import FlatButton from 'material-ui/FlatButton'
 import Dialog from 'material-ui/Dialog'
 
+import { deleteExercise, cancelDeleteExercise } from './ExercisesActions'
+import { showSnackbar } from '../app/AppActions.js'
+
 class ExerciseDeleteDialog extends Component {
+    handleDeleteClick = () => {
+        this.props.deleteExercise(this.props.exercise.id)
+            .then((response) => {
+                this.props.showSnackbar('Deleted Exercise \'' + this.props.exercise.name + '\'.')
+                this.props.handleClose();
+            }, (error) => {
+                this.handleApiError(error);
+            })
+    }
+
+    handleApiError = (error) => {
+        let message = 'Error deleting Exercise'
+
+        if (error.response) {
+            message += ': ' + JSON.stringify(error.response.data).replace(/"/g, "")
+        }
+        else {
+            message += '.'
+        }
+
+        this.props.showSnackbar(message);
+    }
+
     render() {
         const actions = [
             <FlatButton
               label="Cancel"
-              onClick={() => this.props.handleClose({ cancelled: true })}
+              onClick={() => this.props.handleClose()}
             />,
             <FlatButton
               label="Delete"
-              onClick={() => this.props.handleClose({ deleted: true })}
+              onClick={() => this.handleDeleteClick()}
             />,
           ];
 
@@ -31,5 +58,15 @@ class ExerciseDeleteDialog extends Component {
     }
 }
 
-export default ExerciseDeleteDialog
+const mapStateToProps = (state) => ({
+    api: state.exercises.api
+})
+
+const mapDispatchToProps = {
+    deleteExercise,
+    cancelDeleteExercise,
+    showSnackbar
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ExerciseDeleteDialog)
 
