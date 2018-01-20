9133706877e5340219cb314f3cf95a60d5e5bfbc
JP Dillingham <jpdillingham@gmail.com>
2018-01-20 16:51:01 -0600

    Implemented rough draft of Routines based on Exercises

--- src/components/routines/Routines.js
+++ src/components/routines/Routines.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { fetchRoutines } from './RoutinesActions'
+
+import { red500 } from 'material-ui/styles/colors'
+import CircularProgress from 'material-ui/CircularProgress'
+import ActionHighlightOff from 'material-ui/svg-icons/action/highlight-off'
+
+const styles = {
+    grid: {
+        display: 'grid',
+        gridGap: 10,
+        gridTemplateColumns: 'repeat(auto-fit, 400px)'
+    },
+    icon: {
+        height: 48,
+        width: 48,
+        position: 'fixed',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)'
+    },
+}
+
 class Routines extends Component {
     state = {
         api: {
@@ -9,19 +31,47 @@ class Routines extends Component {
         }
     }
 
+    componentWillMount() {
+        this.setState({ api: { ...this.state.api, isExecuting: true }})
+
+        this.props.fetchRoutines()
+            .then(response => {
+                this.setState({ api: { isExecuting: false, isErrored: false }})
+            }, error => {
+                this.setState({ api: { isExecuting: false, isErrored: true }})
+            })
+    }
+
     render() {
         return (
             <div>
-                Routines
+                { 
+                    this.state.api.isExecuting ? <CircularProgress style={styles.icon} /> : 
+                        this.state.api.isErrored ? <ActionHighlightOff style={{ ...styles.icon, color: red500 }} /> :
+                            <div style={styles.grid}>
+                                {this.props.routines.map(r =>  
+                                    <div key={r.id}>
+                                        {r.name}
+                                        <ul>
+                                            {r.exercises.map(e =>
+                                                <li>{e.name}</li>
+                                            )}
+                                        </ul>
+                                    </div>
+                                )}
+                            </div>
+                }
             </div>
         )
     }
 } 
 
 const mapStateToProps = (state) => ({
+    routines: state.routines,
 })
 
 const mapDispatchToProps = {
+    fetchRoutines,
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Routines)
