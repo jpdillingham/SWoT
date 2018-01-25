9b2b30b325314d16398c2b4614a1056f39b42ff6
JP Dillingham <jpdillingham@gmail.com>
2018-01-24 20:55:28 -0600

    Simplified reducers

--- src/reducers.js
+++ src/reducers.js
@@ -1,23 +1,7 @@
 import { combineReducers } from 'redux'
-import { ROUTINES } from './constants'
 import ExercisesReducer from './components/exercises/ExercisesReducer'
 import AppReducer from './components/app/AppReducer';
 import RoutinesReducer from './components/routines/RoutinesReducer'
-
-const initialState = {
-    routines: ROUTINES,
-    snackbar: {
-        visible: false,
-        message: ''
-    }
-}
-
-const routines = (state = initialState.routines, action) => {
-    switch (action.type) {
-        default:
-            return state;
-    }
-}
 
 const rootReducer = combineReducers({ 
     app: AppReducer, 
