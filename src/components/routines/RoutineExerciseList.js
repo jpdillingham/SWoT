621005d5a311799a59b7f162bc217c9f3a332d1c
jpdillingham <jpdillingham@gmail.com>
2018-02-01 20:40:43 -0600

    Added images to exercise list within routine dialog

--- src/components/routines/RoutineExerciseList.js
+++ src/components/routines/RoutineExerciseList.js
@@ -10,15 +10,38 @@ import ActionAssessment from 'material-ui/svg-icons/action/assessment';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import Divider from 'material-ui/Divider';
 
+import { EXERCISE_TYPES } from '../../constants';
+
+const styles = {
+    leftIcon: {
+        width: '32px',
+        height: '32px',
+        marginTop: 7,
+    }
+}
+
 class RoutineExerciseList extends Component {
+    getImage = (type) => {
+        if (EXERCISE_TYPES.indexOf(type) === -1) { 
+            type = 'unknown'
+        }
+    }
+
     render() {
+
+
         return (
             <List>
                 <Subheader>Exercises</Subheader>
                 {this.props.exercises ? this.props.exercises.map(e =>                     
                         <ListItem
                             key={e.id}
-                            leftIcon={<ActionAssessment/>}
+                            leftIcon={                        
+                            <img alt={e.type} 
+                                style={styles.leftIcon} 
+                                src={process.env.PUBLIC_URL + '/img/' + e.type + '.png'}
+                            />
+                            }
                             rightIconButton={
                                 <IconMenu iconButtonElement={
                                     <IconButton touch={true} tooltipPosition="bottom-left">
