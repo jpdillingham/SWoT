b4fba45201b2f6aac0a3017f560ce092ec2ab1f4
JP Dillingham <jpdillingham@gmail.com>
2018-02-05 21:18:05 -0600

    Completed routines reducer

--- src/components/routines/RoutinesReducer.js
+++ src/components/routines/RoutinesReducer.js
@@ -4,6 +4,12 @@ const RoutinesReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'ROUTINES_GET':
             return action.routines
+        case 'ROUTINES_POST':
+            return state.concat(action.routine)
+        case 'ROUTINES_PUT': 
+            return state.map(r => {
+                return r.id === action.routine.id ? action.routine : r
+            })
         case 'ROUTINES_DELETE':
             return state.filter(r => r.id !== action.id)
         default:
