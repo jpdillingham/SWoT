9de8fb667e0e98916af250e6d40671ca1d608ae6
JP Dillingham <jpdillingham@gmail.com>
2018-02-18 19:33:06 -0600

    Added form validation

--- src/components/security/RegisterCard.js
+++ src/components/security/RegisterCard.js
@@ -34,7 +34,70 @@ const styles = {
     }
 }
 
+const initialState = {
+    info: {
+        email: undefined,
+        password: undefined,
+        password2: undefined,
+    },
+    validationErrors: {
+        email: undefined,
+        password: undefined,
+        password2: undefined,
+    }
+}
+
 class RegisterCard extends Component {
+    state = initialState;
+
+    handleEmailChange = (event, value) => {
+        this.setState({ info: { ...this.state.info, email: value }});
+    }
+
+    handlePasswordChange = (event, value) => {
+        this.setState({ 
+            info: { ...this.state.info, password: value },
+            validationErrors: { ...this.state.validationErrors, password: undefined, password2: undefined }
+        });
+    }
+
+    handlePassword2Change = (event, value) => {
+        this.setState({ 
+            info: { ...this.state.info, password2: value },
+            validationErrors: { ...this.state.validationErrors, password: undefined, password2: undefined }
+        });
+    }
+
+    handleRegisterClick = () => {
+        this.setState({ validationErrors: this.validateState() }, () => {
+            if (Object.keys(this.state.validationErrors).find(e => this.state.validationErrors[e] !== '') === undefined) {
+                this.props.onRegisterClick(this.state.email, this.state.password);
+            }
+        })
+    }
+
+    validateState = () => {
+        let validationErrors = this.state.validationErrors;
+
+        if (this.state.info.password === undefined || this.state.info.password === '') {
+            validationErrors = { 
+                ...validationErrors, 
+                password: 'The password can\'t be blank.' 
+            }
+        }
+        else if (this.state.info.password !== this.state.info.password2) {
+            let text = 'The supplied passwords don\'t match.';
+
+            validationErrors = { 
+                ...validationErrors, 
+                password: text,
+                password2: text,
+            }
+        }
+
+        return validationErrors;
+    }
+
     render() {
         return(
             <div>
@@ -44,6 +107,7 @@ class RegisterCard extends Component {
                     <TextField
                         hintText="Email"
                         floatingLabelText="Email"
+                        onChange={this.handleEmailChange}
                     />
                 </div>
                 <div style={styles.group}>
@@ -51,6 +115,9 @@ class RegisterCard extends Component {
                     <TextField
                         hintText="Password"
                         floatingLabelText="Password"
+                        defaultValue={this.state.info.password}
+                        errorText={this.state.validationErrors.password}
+                        onChange={this.handlePasswordChange}
                     />
                 </div>
                 <div style={styles.group}>
@@ -58,6 +125,9 @@ class RegisterCard extends Component {
                     <TextField
                         hintText="Repeat Password"
                         floatingLabelText="Repeat Password"
+                        defaultValue={this.state.info.password2}
+                        errorText={this.state.validationErrors.password2}
+                        onChange={this.handlePassword2Change}
                     />
                 </div>
                 </CardText>
@@ -67,7 +137,7 @@ class RegisterCard extends Component {
                             style={styles.button} 
                             primary={true} 
                             label="Register" 
-                            onClick={() => this.props.onRegisterClick('user', 'test@whatnet.us', 'password!')} />
+                            onClick={this.handleRegisterClick} />
                     </div>
                     <div style={styles.center}>
                         <span style={styles.toggleText}>Already registered?</span>
