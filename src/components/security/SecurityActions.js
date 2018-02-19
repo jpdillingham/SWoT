6d0ed15f84e259e021ce85c687da29e7e9f6a62f
JP Dillingham <jpdillingham@gmail.com>
2018-02-18 18:40:00 -0600

    Stubbed register action

--- src/components/security/SecurityActions.js
+++ src/components/security/SecurityActions.js
@@ -1,3 +1,7 @@
+import { CognitoUserPool, CognitoUserAttribute, CognitoUser } from 'amazon-cognito-identity-js'
+
+import { COGNITO_DATA } from '../../constants'
+
 export const login = (user) => ({
     type: 'LOGIN',
     user: user
@@ -6,3 +10,27 @@ export const login = (user) => ({
 export const logout = () => ({
     type: 'LOGOUT'
 })
+
+export const register = (username, email, password) => (dispatch) => {
+    let userPool = new CognitoUserPool(COGNITO_DATA);
+    
+    let email = {
+        Name: 'email',
+        Value: email,
+    }
+    
+    let attributeList = [];
+    attributeList.push(new CognitoUserAttribute(email));
+
+    return new Promise((resolve, reject) => { 
+        userPool.signUp(username, password, attributeList, null, function(err, result) {
+            if (err) {
+                console.log(err);
+                reject(err);
+            }
+            else {
+                console.log(result);
+                resolve(result);
+            }
+        })
+}
