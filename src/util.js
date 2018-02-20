846913de3b4370fd4fb7c3e6151aeb056e396903
JP Dillingham <jpdillingham@gmail.com>
2018-02-19 18:37:08 -0600

    Added verifyEmail()

--- src/util.js
+++ src/util.js
@@ -17,3 +17,8 @@ export const swap = (array, index1, index2) => {
 
     return copy;
 }
+
+export const validateEmail = (email) => {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
