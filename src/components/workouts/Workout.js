1078981f6bdaae2b1950b8ebf599410642d78f33
JP Dillingham <jpdillingham@gmail.com>
2018-02-28 20:30:00 -0600

    Renamed main to workouts

--- src/components/workouts/Workout.js
+++ src/components/workouts/Workout.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 
 import Navigation from '../shared/Navigation'
 
-class Main extends Component {
+class Workouts extends Component {
     render() {
         return (
             <Navigation>
-                <h1>Home</h1>
+                <h1>Workouts</h1>
             </Navigation>
         )
     }
 }
 
-export default Main
+export default Workouts
