1a2ae39e8167584de4177060a5c62c0d55fe9529
JP Dillingham <jpdillingham@gmail.com>
2018-03-03 16:46:43 -0600

    Removed unused import

--- src/components/app/Links.js
+++ src/components/app/Links.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 import { Link } from 'react-router-dom';
 import MenuItem from 'material-ui/MenuItem'
-import Home from 'material-ui/svg-icons/action/home';
 import ActionAssignment from 'material-ui/svg-icons/action/assignment';
 import ActionAssignmentTurnedIn from 'material-ui/svg-icons/action/assignment-turned-in';
 import ActionDescription from 'material-ui/svg-icons/action/description'
