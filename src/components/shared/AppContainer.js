424609746c2bab36b0f662f3611b601dd168f79a
JP Dillingham <jpdillingham@gmail.com>
2018-03-03 17:14:58 -0600

    Renamed Navigation to AppContainer

---
+++ src/components/shared/AppContainer.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import LogoutButton from '../security/LogoutButton';
+
+import Links from '../app/Links'
+
+class AppContainer extends Component {
+    state = {
+        drawerOpen: false,
+    }
+
+    toggleDrawer = () => {
+        this.setState({ drawerOpen: !this.state.drawerOpen })
+    }
+
+    render() {
+        return (
+            <div>
+                {this.props.show ? 
+                    <div>
+                <AppBar 
+                    title="SWoT" 
+                    style={styles.appBar}
+                    onLeftIconButtonClick={this.toggleDrawer}
+                >
+                    <LogoutButton/>
+                </AppBar>
+                <Drawer 
+                    open={this.state.drawerOpen}
+                    docked={false}
+                    onRequestChange={this.toggleDrawer}
+                >
+                    <AppBar title="SWoT" showMenuIconButton={false}/>
+                    <Links/>
+                </Drawer>
+                </div>
+                : ''}
+                    {this.props.children}
+            </div>
+        );
+    }
+}
+
+const styles = {
+    appBar: {
+        position: 'fixed',
+        left: 0,
+        top: 0,
+    }
+}
+
+export default AppContainer;
