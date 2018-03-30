8019b65be577ee2205621330f54e0341062d3c2e
JP Dillingham <jpdillingham@gmail.com>
2018-03-30 16:05:03 -0500

    Added action creators and reducers for workouts

---
+++ src/components/workouts/WorkoutsReducers.js
@@ -0,0 +1,18 @@
+const initialState = []
+
+const WorkoutsReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'WORKOUTS_GET':
+            return action.workouts
+        case 'WORKOUTS_POST':
+            return state.concat(action.workout)
+        case 'WORKOUTS_PUT': 
+            return state.map(w => {
+                return w.id === action.workout.id ? action.workout : w
+            })
+        case 'WORKOUTS_DELETE':
+            return state.filter(w => w.id !== action.id)
+        default:
+            return state;
+    }
+}
