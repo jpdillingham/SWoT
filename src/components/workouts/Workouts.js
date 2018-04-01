bf925396cbb662789daaf9d13c741f7dc4916299
JP Dillingham <jpdillingham@gmail.com>
2018-03-31 19:30:58 -0500

    Experimenting with GridList

--- src/components/workouts/Workouts.js
+++ src/components/workouts/Workouts.js
@@ -6,6 +6,7 @@ import Paper from 'material-ui/Paper'
 import AddFloatingAddButton from '../shared/AddFloatingActionButton'
 import WorkoutDialog from './WorkoutDialog';
 import ActionDateRange from 'material-ui/svg-icons/action/date-range'
+import { GridList, GridTile } from 'material-ui/GridList'
 import { black } from 'material-ui/styles/colors'
 
 import { WORKOUT_AVATAR_COLOR } from '../../constants'
@@ -20,13 +21,18 @@ const styles = {
         marginTop: 6,
     },
     card: {
-        width: '390px',
+        width: '100%',
         height: '100%',
         position: 'relative'
     },
     text: {
         /* marginBottom: 40 */
     },
+    tile: {
+        border: '1px solid black',
+        width: '100px',
+        height: '100px'
+    }
 }
 
 class Workouts extends Component {
@@ -41,6 +47,19 @@ class Workouts extends Component {
                         avatar={<Avatar backgroundColor={WORKOUT_AVATAR_COLOR} color={black} size={36} icon={<ActionDateRange/>}></Avatar>}
                     />
                     <CardText style={styles.text}>
+                        <GridList 
+                            cellHeight={'auto'}
+                            cols={7}
+                            padding={0}
+                        >
+                            <GridTile style={styles.tile}>1</GridTile>
+                            <GridTile style={styles.tile}>2</GridTile>
+                            <GridTile style={styles.tile}>3</GridTile>
+                            <GridTile style={styles.tile}>4</GridTile>
+                            <GridTile style={styles.tile}>5</GridTile>
+                            <GridTile style={styles.tile}>6</GridTile>
+                            <GridTile style={styles.tile}>7</GridTile>
+                        </GridList>
                     </CardText>
                 </Card>
                 <AddFloatingAddButton dialog={<WorkoutDialog/>}/>
