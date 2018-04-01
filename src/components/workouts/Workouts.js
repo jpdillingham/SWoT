8f4e0c57003a4f401705ccedb83038ebc42a348c
JP Dillingham <jpdillingham@gmail.com>
2018-03-31 19:16:12 -0500

    Began working on workout UI

--- src/components/workouts/Workouts.js
+++ src/components/workouts/Workouts.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
 
+import {Card, CardHeader, CardText } from 'material-ui/Card';
+import Avatar from 'material-ui/Avatar'
+import Paper from 'material-ui/Paper'
+import AddFloatingAddButton from '../shared/AddFloatingActionButton'
+import WorkoutDialog from './WorkoutDialog';
+import ActionDateRange from 'material-ui/svg-icons/action/date-range'
+import { black } from 'material-ui/styles/colors'
+
+import { WORKOUT_AVATAR_COLOR } from '../../constants'
+
+const styles = {
+    cardHeader: {
+        backgroundColor: WORKOUT_AVATAR_COLOR,
+        marginBottom: 0,
+    },
+    cardTitle: {
+        fontSize: '20px',
+        marginTop: 6,
+    },
+    card: {
+        width: '390px',
+        height: '100%',
+        position: 'relative'
+    },
+    text: {
+        /* marginBottom: 40 */
+    },
+}
+
 class Workouts extends Component {
     render() {
         return (
             <div>
-                <h1>Workouts</h1>
+                <Card zDepth={2} style={styles.card}>
+                    <CardHeader
+                        title={'Past Workouts'}
+                        titleStyle={styles.cardTitle}
+                        style={styles.cardHeader}
+                        avatar={<Avatar backgroundColor={WORKOUT_AVATAR_COLOR} color={black} size={36} icon={<ActionDateRange/>}></Avatar>}
+                    />
+                    <CardText style={styles.text}>
+                    </CardText>
+                </Card>
+                <AddFloatingAddButton dialog={<WorkoutDialog/>}/>
             </div>
         )
     }
