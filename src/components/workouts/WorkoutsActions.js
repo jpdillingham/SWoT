c5b15d5625789bc920dbad2692b97acb2381335d
JP Dillingham <jpdillingham@gmail.com>
2018-04-03 20:29:37 -0500

    Implemented fetch and add

--- src/components/workouts/WorkoutsActions.js
+++ src/components/workouts/WorkoutsActions.js
@@ -21,3 +21,32 @@ const workoutsDelete = (id) => ({
     type: 'WORKOUTS_DELETE',
     id: id
 })
+
+export const fetchWorkouts = () => (dispatch, getState) => {
+    return new Promise((resolve, reject) => {
+        api.get(endpoint)
+        .then(response => {
+            dispatch(workoutsGet(response.data));
+            resolve(response);
+        }, error => {
+            reject('API error: ' + error);
+        });    
+    });
+}
+
+export const addWorkout = (workout) => (dispatch, getState) => {
+    return new Promise((resolve, reject) => {
+        api.post(endpoint, workout)
+        .then(response => {
+            if (response.status === 201) {
+                dispatch(workoutsPost(response.data))
+                resolve(response)
+            }
+            else {
+                reject("Unknown POST response code (expected 201, received " + response.status + ").")
+            }            
+        }, error => {
+            reject('API error: ' + error);
+        });
+    });
+}
