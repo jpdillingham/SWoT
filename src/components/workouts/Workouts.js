82f2faa66ba190327ac8bbd52958b4841003c9f3
JP Dillingham <jpdillingham@gmail.com>
2018-04-06 22:10:24 -0500

    Removed unused imports

--- src/components/workouts/Workouts.js
+++ src/components/workouts/Workouts.js
@@ -3,8 +3,7 @@ import { connect } from 'react-redux';
 
 import { fetchWorkouts } from '../workouts/WorkoutsActions'
 
-import { Card, CardHeader, CardText, CardActions } from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton'
+import { Card, CardHeader, CardText } from 'material-ui/Card';
 import Avatar from 'material-ui/Avatar'
 import AddFloatingAddButton from '../shared/AddFloatingActionButton'
 import WorkoutDialog from './WorkoutDialog';
