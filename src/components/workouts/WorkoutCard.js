d7b5b1114393a6beb01f0b8a6679d8990f67b2fe
JP Dillingham <jpdillingham@gmail.com>
2018-04-07 22:30:47 -0500

    Stubbed expand event handler

--- src/components/workouts/WorkoutCard.js
+++ src/components/workouts/WorkoutCard.js
@@ -3,16 +3,22 @@ import React, { Component } from 'react'
 import { Card, CardHeader, CardText } from 'material-ui/Card'
 
 class WorkoutCard extends Component {
+    handleExpandChange = (expanded) => {
+        console.log(expanded)
+    }
+
     render() {
         return (
-            <Card>
+            <Card onExpandChange={this.handleExpandChange}>
                 <CardHeader
                     title={this.props.title}
                     subtitle={this.props.subtitle}
                     actAsExpander={true}
                     showExpandableButton={true}
                 />
-                <CardText expandable={true}>
+                <CardText 
+                    expandable={true}
+                >
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
                     Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
