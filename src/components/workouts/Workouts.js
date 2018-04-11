c7502de288b762f13f57a90ed22f812b7cf2c210
JP Dillingham <jpdillingham@gmail.com>
2018-04-11 06:50:07 -0500

    Removed unused stuff

--- src/components/workouts/Workouts.js
+++ src/components/workouts/Workouts.js
@@ -3,40 +3,10 @@ import { connect } from 'react-redux';
 
 import { fetchWorkouts } from '../workouts/WorkoutsActions'
 
-import { Card, CardHeader } from 'material-ui/Card';
-import Avatar from 'material-ui/Avatar'
 import AddFloatingAddButton from '../shared/AddFloatingActionButton'
 import WorkoutDialog from './WorkoutDialog';
-import ActionRestore from 'material-ui/svg-icons/action/restore'
-import { black } from 'material-ui/styles/colors'
 
-import { WORKOUT_AVATAR_COLOR } from '../../constants'
-import WorkoutCard from './WorkoutCard';
 import WorkoutResumeCard from './WorkoutResumeCard'
-
-const styles = {
-    cardHeader: {
-        backgroundColor: WORKOUT_AVATAR_COLOR,
-        marginBottom: 0,
-    },
-    cardTitle: {
-        fontSize: '20px',
-        marginTop: 6,
-    },
-    card: {
-        width: '100%',
-        height: '100%',
-        position: 'relative',
-    },
-    text: {
-        padding: '0px',
-    },
-    tile: {
-        border: '1px solid black',
-        width: '100px',
-        height: '100px'
-    }
-}
 
 const initialState = {
     api: {
