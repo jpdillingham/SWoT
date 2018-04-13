cd4ce22bb2ed2aa76fbebbe01a9bc4ec2ef15375
JP Dillingham <jpdillingham@gmail.com>
2018-04-13 07:42:36 -0500

    import fetchWorkouts

--- src/components/workouts/Workout.js
+++ src/components/workouts/Workout.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { fetchWorkouts } from '../workouts/WorkoutsActions'
+
 class Workout extends Component {
     render() {
         console.log(this.props.match.params)
