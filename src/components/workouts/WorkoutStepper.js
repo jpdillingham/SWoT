a083914f4e98247d6b380c0f5b3a88eb8646495b
JP Dillingham <jpdillingham@gmail.com>
2018-04-23 19:07:26 -0500

    add WorkoutStepper

---
+++ src/components/workouts/WorkoutStepper.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+
+import {Card, CardHeader, CardText, CardActions } from 'material-ui/Card';
+import ActionHistory from 'material-ui/svg-icons/action/history';
+import Avatar from 'material-ui/Avatar';
+
+import { Step, Stepper, StepButton, StepContent } from 'material-ui/Stepper';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import TextField from 'material-ui/TextField';
+import ActionCheckCircle from 'material-ui/svg-icons/action/check-circle'
+import AVPlayCircleFilled from 'material-ui/svg-icons/av/play-circle-filled'
+import ImageLens from 'material-ui/svg-icons/image/lens'
+
+import WorkoutExerciseForm from './WorkoutExerciseForm'
+
+import SaveRetryFlatButton from '../shared/SaveRetryFlatButton';
+
+import { CARD_WIDTH, EXERCISE_TYPES, EXERCISE_AVATAR_COLOR } from '../../constants';
+import FlatButton from 'material-ui/FlatButton/FlatButton';
+
+const styles = {
+    container: {
+        height: '100%'
+    },
+    cardHeader: {
+        backgroundColor: EXERCISE_AVATAR_COLOR,
+        marginBottom: 0,
+    },
+    cardTitle: {
+        fontSize: '20px',
+        marginTop: 6,
+    },
+    iconMenu: {
+        position: 'absolute',
+        right: 0,
+        top: 10,
+    },
+    card: {
+        width: CARD_WIDTH - 40,
+        height: '100%',
+        position: 'relative',
+        marginBottom: 5,
+        marginLeft: 5,
+        marginTop: 20
+    },
+    fab: {
+        margin: 0,
+        top: 47,
+        right: 20,
+        bottom: 'auto',
+        left: 'auto',
+        position: 'absolute',
+        zIndex: 1000,
+    },
+    link: {
+        cursor: 'pointer',
+    },
+    button: {
+        float: 'right',
+    },
+}
+
+const initialState = {
+    stepIndex: 0,
+}
+
+class WorkoutStepper extends Component {
+    state = initialState;
+
+    render() {
+        return (
+            <Stepper
+                activeStep={this.state.stepIndex}
+                linear={false}
+                orientation={'vertical'}
+            >
+                {this.props.workout.routine.exercises.map((exercise, index) =>
+                    <Step key={index}>
+                        <StepButton 
+                            completed={exercise.endTime !== undefined}
+                            onClick={() => this.handleStepClick(index)}
+                            icon={exercise.endTime !== undefined ? 
+                                <ActionCheckCircle/> :
+                                exercise.startTime !== undefined ?
+                                    <AVPlayCircleFilled/> :
+                                    <ImageLens/>
+                            }
+                        >
+                            {exercise.name}
+                        </StepButton>
+                        <StepContent>
+                            <WorkoutExerciseForm 
+                                stepIndex={index}
+                                exercise={exercise}
+                                onChange={this.props.onExerciseChange}
+                                onComplete={this.props.onExerciseComplete}
+                            />
+                        </StepContent>
+                    </Step>
+                )}
+            </Stepper>
+        )
+    }
+}
+
+export default WorkoutStepper
