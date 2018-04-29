3850e5cfd42bcc6325fe5aeb5e679c7c98becfd3
JP Dillingham <jpdillingham@gmail.com>
2018-04-28 22:57:32 -0500

    cleanup, rename swap to be less ambiguous swapArrayElements

--- src/util.js
+++ src/util.js
@@ -1,29 +1,3 @@
-export const getGuid = () => {
-    function s4() {
-        return Math.floor((1 + Math.random()) * 0x10000)
-        .toString(16)
-        .substring(1);
-    }
-    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-        s4() + '-' + s4() + s4() + s4();
-}
-
-export const swap = (array, index1, index2) => {
-    let copy = array.slice();
-
-    let temp = copy[index1];
-    copy[index1] = copy[index2];
-    copy[index2] = temp;
-
-    return copy;
-}
-
-export const validateEmail = (email) => {
-    // eslint-disable-next-line
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
-
 export const getElapsedTime = (start, end) => {
     end = end || new Date().getTime();
     let duration = Math.trunc((end - start) / 1000);
@@ -40,4 +14,30 @@ export const getElapsedTime = (start, end) => {
     }
 
     return formatTime(duration);
+}
+
+export const getGuid = () => {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
+}
+
+export const swapArrayElements = (array, index1, index2) => {
+    let copy = array.slice();
+
+    let temp = copy[index1];
+    copy[index1] = copy[index2];
+    copy[index2] = temp;
+
+    return copy;
+}
+
+export const validateEmail = (email) => {
+    // eslint-disable-next-line
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
