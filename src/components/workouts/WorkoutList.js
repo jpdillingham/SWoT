2d8d582056380633fd36048e4d993a5dcc827a4d
JP Dillingham <jpdillingham@gmail.com>
2018-04-30 07:31:38 -0500

    remove extra div in WorkoutList

--- src/components/workouts/WorkoutList.js
+++ src/components/workouts/WorkoutList.js
@@ -28,44 +28,42 @@ const styles = {
 class WorkoutList extends Component {
     render() {
         return (
-            <div>
-                {this.props.workouts && this.props.workouts.length > 0 ? 
-                    <Card zDepth={2} style={styles.card}>
-                        <CardHeader
-                            title={this.props.title}
-                            titleStyle={styles.cardTitle}
-                            style={styles.cardHeader}
-                            avatar={<Avatar backgroundColor={WORKOUT_AVATAR_COLOR} color={black} size={36} icon={this.props.icon}></Avatar>}
-                        />
-                        <CardText>
-                            <List>
-                                {this.props.workouts
-                                    .sort((a, b) => { 
-                                        let f = this.props.timeField;
-                                        if (this.props.sort === 'desc') {
-                                            return a[f] > b[f] ? -1 : 
-                                                a[f] === b[f] ? 0 : 1 
-                                        }
-                                        else {
-                                            return a[f] < b[f] ? -1 : 
-                                                a[f] === b[f] ? 0 : 1 
-                                        }
-                                    })
-                                    .map(w => 
-                                    <ListItem
-                                        key={w.id}
-                                        primaryText={w.routine.name}
-                                        secondaryText={this.props.timePrefix + ' ' + moment(w[this.props.timeField]).calendar()}
-                                        leftIcon={<ActionAssignmentTurnedIn/>}
-                                        rightIcon={this.props.itemRightIcon}
-                                        onClick={() => this.props.onClick(w.id)}
-                                    />
-                                )}
-                            </List>
-                        </CardText>
-                    </Card>
-                : '' }
-            </div>
+            this.props.workouts && this.props.workouts.length > 0 ? 
+                <Card zDepth={2} style={styles.card}>
+                    <CardHeader
+                        title={this.props.title}
+                        titleStyle={styles.cardTitle}
+                        style={styles.cardHeader}
+                        avatar={<Avatar backgroundColor={WORKOUT_AVATAR_COLOR} color={black} size={36} icon={this.props.icon}></Avatar>}
+                    />
+                    <CardText>
+                        <List>
+                            {this.props.workouts
+                                .sort((a, b) => { 
+                                    let f = this.props.timeField;
+                                    if (this.props.sort === 'desc') {
+                                        return a[f] > b[f] ? -1 : 
+                                            a[f] === b[f] ? 0 : 1 
+                                    }
+                                    else {
+                                        return a[f] < b[f] ? -1 : 
+                                            a[f] === b[f] ? 0 : 1 
+                                    }
+                                })
+                                .map(w => 
+                                <ListItem
+                                    key={w.id}
+                                    primaryText={w.routine.name}
+                                    secondaryText={this.props.timePrefix + ' ' + moment(w[this.props.timeField]).calendar()}
+                                    leftIcon={<ActionAssignmentTurnedIn/>}
+                                    rightIcon={this.props.itemRightIcon}
+                                    onClick={() => this.props.onClick(w.id)}
+                                />
+                            )}
+                        </List>
+                    </CardText>
+                </Card>
+            : '' 
         )
     }
 }
