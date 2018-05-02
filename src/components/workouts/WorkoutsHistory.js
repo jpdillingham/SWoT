f6ef20c6a75d633399e844fe14a3bbe9a6a3adec
JP Dillingham <jpdillingham@gmail.com>
2018-05-02 07:36:16 -0500

    add WorkoutsHistory component

---
+++ src/components/workouts/WorkoutsHistory.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { fetchWorkouts } from './WorkoutsActions'
+import { fetchWorkoutsHistory } from './WorkoutsHistoryActions'
+
+import { red500 } from 'material-ui/styles/colors'
+import CircularProgress from 'material-ui/CircularProgress'
+import ActionHighlightOff from 'material-ui/svg-icons/action/highlight-off'
+import FlatButton from 'material-ui/FlatButton'
+import Divider from 'material-ui/Divider'
+
+import AddFloatingAddButton from '../shared/AddFloatingActionButton'
+import WorkoutDialog from './WorkoutDialog';
+import ActionSchedule from 'material-ui/svg-icons/action/schedule'
+import ActionDone from 'material-ui/svg-icons/action/done'
+import AVPlayArrow from 'material-ui/svg-icons/av/play-arrow'
+import ActionInfo from 'material-ui/svg-icons/action/info'
+
+import HardwareKeyboardArrowLeft from 'material-ui/svg-icons/hardware/keyboard-arrow-left'
+import HardwareKeyboardArrowRight from 'material-ui/svg-icons/hardware/keyboard-arrow-right'
+
+import WorkoutListCard from './WorkoutListCard'
+
+const initialState = {
+    api: {
+        isExecuting: false,
+        isErrored: false,
+    }
+}
+
+const styles = {
+    grid: {
+        display: 'grid',
+        gridGap: 10,
+    },
+    icon: {
+        height: 48,
+        width: 48,
+        position: 'fixed',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)'
+    },
+}
+
+class WorkoutsHistory extends Component {
+    state = initialState;
+
+    componentWillMount() {
+        this.setState({ api: { ...this.state.api, isExecuting: true }})
+
+        this.props.fetchWorkoutsHistory(10, 0)
+        .then(response => {
+            this.setState({ api: { isExecuting: false, isErrored: false }})
+        }, error => {
+            this.setState({ api: { isExecuting: false, isErrored: true }})
+        })
+    }
+
+    navigate = (url) => {
+        this.props.history.push(url);
+    }
+
+    handleClick = (workoutId) => {
+        this.navigate('/workouts/' + workoutId)
+    }
+
+    render() {
+        return (
+            this.state.api.isExecuting ? <CircularProgress style={styles.icon} /> : 
+                this.state.api.isErrored ? <ActionHighlightOff style={{ ...styles.icon, color: red500 }} /> :
+                    <div style={styles.grid}>
+                        <WorkoutListCard 
+                            title={'Completed'}
+                            icon={<ActionDone/>}
+                            itemRightIcon={<ActionInfo/>}
+                            workouts={this.props.workoutsHistory.workouts}
+                            sort={'desc'}
+                            timePrefix={'Completed'}
+                            timeField={'endTime'}
+                            onClick={this.handleClick}
+                        >
+                            <Divider style={{ marginBottom: 20 }}/>
+                            <FlatButton icon={<HardwareKeyboardArrowLeft/>}/>
+                            <FlatButton icon={<HardwareKeyboardArrowRight/>}/>
+                        </WorkoutListCard>
+                    </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => ({
+    workoutsHistory: state.workoutsHistory,
+})
+
+const mapDispatchToProps = {
+    fetchWorkoutsHistory
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(WorkoutsHistory)
