8d0c19e098db6b60f6ff960a16970da498572cfa
JP Dillingham <jpdillingham@gmail.com>
2018-05-03 08:08:29 -0500

    add WorkoutsHistoryOptions

---
+++ src/components/workouts/WorkoutsHistoryOptions.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { fetchWorkoutsHistory } from './WorkoutsHistoryActions'
+
+import { black, red500 } from 'material-ui/styles/colors'
+import CircularProgress from 'material-ui/CircularProgress'
+import ActionHighlightOff from 'material-ui/svg-icons/action/highlight-off'
+import FlatButton from 'material-ui/FlatButton'
+
+import ActionDone from 'material-ui/svg-icons/action/done'
+import ActionInfo from 'material-ui/svg-icons/action/info'
+import HardwareKeyboardArrowLeft from 'material-ui/svg-icons/hardware/keyboard-arrow-left'
+import HardwareKeyboardArrowRight from 'material-ui/svg-icons/hardware/keyboard-arrow-right'
+
+import DropDownMenu from 'material-ui/DropDownMenu'
+import MenuItem from 'material-ui/MenuItem'
+
+import WorkoutListCard from './WorkoutListCard'
+import SelectField from 'material-ui/SelectField/SelectField';
+
+const limitOptions = [ 5, 10, 25, 50 ];
+
+const styles = {
+}
+
+class WorkoutsHistoryOptions extends Component {
+    handleLimitChange = (event, index, value) => {
+        this.props.onChange({ ...this.props.filters, limit: value });
+    }
+
+    render() {
+        return (
+            <SelectField value={this.props.filters.limit} onChange={this.handleLimitChange}>
+                {limitOptions.map((o, index) => 
+                    <MenuItem key={index} value={o} primaryText={o} />                    
+                )}
+            </SelectField>
+        )
+    }
+}
+
+export default WorkoutsHistoryOptions
