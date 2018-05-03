231c39cecee9f0898cc282346ad8f253dd2f527c
JP Dillingham <jpdillingham@gmail.com>
2018-05-02 22:04:08 -0500

    fetch x-total-count header and populate totalCount in workouts history get action

--- src/components/workouts/WorkoutsHistoryActions.js
+++ src/components/workouts/WorkoutsHistoryActions.js
@@ -3,10 +3,11 @@ import { API_ROOT } from "../../constants"
 
 const endpoint = API_ROOT + '/workouts';
 
-const workoutsHistoryGet = (workouts, filters) => ({
+const workoutsHistoryGet = (workouts, filters, totalCount) => ({
     type: 'WORKOUTS_HISTORY_GET',
     workouts: workouts,
     filters: filters,
+    totalCount: totalCount,
 })
 
 export const fetchWorkoutsHistory = (filters) => (dispatch, getState) => {
@@ -20,7 +21,8 @@ export const fetchWorkoutsHistory = (filters) => (dispatch, getState) => {
     return new Promise((resolve, reject) => {
         api.get(endpoint + queryParams)
         .then(response => {
-            dispatch(workoutsHistoryGet(response.data, filters));
+            console.log(response)
+            dispatch(workoutsHistoryGet(response.data, filters, parseInt(response.headers['x-total-count'])));
             resolve(response);
         }, error => {
             reject('API error: ' + error);
