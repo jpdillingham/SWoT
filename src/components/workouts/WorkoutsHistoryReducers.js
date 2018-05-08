9ec9375ee9e23ae116a14f80331c88d05f9fdd76
JP Dillingham <jpdillingham@gmail.com>
2018-05-08 08:11:03 -0500

    add upsert for single workout history fetch

--- src/components/workouts/WorkoutsHistoryReducers.js
+++ src/components/workouts/WorkoutsHistoryReducers.js
@@ -4,6 +4,12 @@ const WorkoutsHistoryReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'WORKOUTS_HISTORY_GET':
             return { workouts: action.workouts, totalCount: action.totalCount }
+        case 'WORKOUT_HISTORY_GET':
+            return state.workouts && state.workouts.find(w => w.id === action.workout.id) ?
+                state.workouts.map(w => {
+                    return w.id === action.workout.id ? action.workout : w
+                }) :
+                (state.workouts || []).concat(action.workout);
         default:
             return state;
     }
