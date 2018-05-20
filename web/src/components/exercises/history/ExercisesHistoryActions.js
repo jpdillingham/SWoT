f4eb043a79eb2ed736934239904bb6c39e10c191
JP Dillingham <jpdillingham@gmail.com>
2018-05-20 18:41:53 -0500

    remove single exercise action/reducer

---
+++ web/src/components/exercises/history/ExercisesHistoryActions.js
@@ -0,0 +1,32 @@
+import api from '../../../api';
+import { API_ROOT } from "../../../constants"
+
+const endpoint = API_ROOT + '/exercises/history';
+
+const exercisesHistoryGet = (exercises, totalCount) => ({
+    type: 'EXERCISES_HISTORY_GET',
+    exercises: exercises,
+    totalCount: totalCount,
+});
+
+export const fetchexercisesHistory = (filters) => (dispatch, getState) => {
+    let queryParams = '?';
+    queryParams += 'status=done';
+
+    Object.keys(filters).forEach(f => {
+        if (filters[f] !== undefined) {
+            queryParams += '&' + f + '=' + filters[f];
+        }
+    })
+
+    return new Promise((resolve, reject) => {
+        api.get(endpoint + queryParams)
+        .then(response => {
+            let totalCount = parseInt(response.headers['x-total-count'], 10)
+            dispatch(exercisesHistoryGet(response.data, totalCount));
+            resolve(response);
+        }, error => {
+            reject('API error: ' + error);
+        });    
+    });
+}
--- web/src/components/exercises/history/ExercisesHistoryReducers.js
+++ web/src/components/exercises/history/ExercisesHistoryReducers.js
@@ -4,12 +4,6 @@ const ExercisesHistoryReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'EXERCISES_HISTORY_GET':
             return { exercises: action.exercises, totalCount: action.totalCount }
-        case 'EXERCISE_HISTORY_GET':
-            return state.exercises && state.exercises.find(e => e.id === action.exercise.id) ?
-                state.exercises.map(e => {
-                    return e.id === action.exercise.id ? action.exercise : e
-                }) :
-                (state.exercises || []).concat(action.exercise);
         default:
             return state;
     }
