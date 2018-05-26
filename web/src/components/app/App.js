0bb93bca29f4f01b7825ad18145b6e986413da53
JP Dillingham <jpdillingham@gmail.com>
2018-05-26 13:03:48 -0500

    remove deprecated AppLinks component

--- web/src/components/app/App.js
+++ web/src/components/app/App.js
@@ -10,7 +10,6 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import Snackbar from 'material-ui/Snackbar'
 
 import AppContainer from './AppContainer'
-import AppLinks from './AppLinks'
 
 import Workouts from '../workouts/Workouts'
 import Workout from '../workouts/Workout'
