38d00536672233afd3c7157d900683e12ceb9c8d
JP Dillingham <jpdillingham@gmail.com>
2018-05-30 21:55:03 -0500

    remove unused imports, cleanup

--- web/src/components/exercises/ExerciseReportCard.js
+++ web/src/components/exercises/ExerciseReportCard.js
@@ -1,28 +1,14 @@
 import React, { Component } from 'react';
 
-import ActionHistory from 'material-ui/svg-icons/action/history';
-import ActionAssessment from 'material-ui/svg-icons/action/assessment'
 import Avatar from 'material-ui/Avatar';
+import { ActionAssessment, ActionWatchLater, ActionSpeakerNotes } from 'material-ui/svg-icons';
 import { black } from 'material-ui/styles/colors';
-import { Card, CardHeader, CardText, CardActions } from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton/FlatButton';
-import FloatingActionButton from 'material-ui/FloatingActionButton';
-import TextField from 'material-ui/TextField';
-import { grey300 } from 'material-ui/styles/colors';
+import { Card, CardHeader, CardText } from 'material-ui/Card';
 import LeftRightListItem from '../shared/LeftRightListItem'
-
-import Spinner from '../shared/Spinner';
+import { List } from 'material-ui/List';
 
 import { CARD_WIDTH, EXERCISE_TYPES, EXERCISE_AVATAR_COLOR } from '../../constants';
 import { getElapsedTime } from '../../util';
-
-import SaveRetryFlatButton from '../shared/SaveRetryFlatButton';
-import ExerciseHistoryDialog from './history/ExerciseHistoryDialog';
-import { List } from 'material-ui/List';
-import ListItem from 'material-ui/List/ListItem';
-import { Table, TableRow, TableRowColumn } from 'material-ui/Table';
-import TableBody from 'material-ui/Table/TableBody';
-import { ActionSchedule, ActionWatchLater, ActionSpeakerNotes } from 'material-ui/svg-icons';
 
 const styles = {
     cardHeader: {
