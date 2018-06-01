d887ffb2ea509c2b7d3b8af82c855a999d8729b2
JP Dillingham <jpdillingham@gmail.com>
2018-06-01 08:02:12 -0500

    add Toggled list item to ExerciseReportCard

--- web/src/components/exercises/ExerciseReportCard.js
+++ web/src/components/exercises/ExerciseReportCard.js
@@ -5,6 +5,7 @@ import { ActionAssessment, ActionWatchLater, ActionSpeakerNotes, NavigationExpan
 import { black } from 'material-ui/styles/colors';
 import { Card, CardHeader, CardText } from 'material-ui/Card';
 import LeftRightListItem from '../shared/LeftRightListItem'
+import ToggledLeftRightListItem from '../shared/ToggledLeftRightListItem'
 import { List } from 'material-ui/List';
 
 import { CARD_WIDTH, EXERCISE_TYPES, EXERCISE_AVATAR_COLOR } from '../../constants';
@@ -102,15 +103,13 @@ class ExerciseReportCard extends Component {
                                 rightText={this.props.exercise.startTime ? getElapsedTime(this.props.exercise.startTime, this.props.exercise.endTime) : ' '} 
                             />
                             {!this.props.exercise.notes ? '' :
-                                <div>
-                                    <LeftRightListItem 
-                                        leftIcon={<ActionSpeakerNotes color={ black }/>} 
-                                        leftText={'Notes'}
-                                        rightIcon={this.state.notes.expanded ? <NavigationExpandLess color={black}/> : <NavigationExpandMore color={black}/>}
-                                        onClick={this.handleNotesToggle}
-                                    />
-                                    {this.state.notes.expanded ? <p style={styles.notes}>{this.props.exercise.notes}</p> : ''}
-                                </div>
+                                <ToggledLeftRightListItem
+                                    leftIcon={<ActionSpeakerNotes color={black}/>}
+                                    leftText={'Notes'}
+                                    defaultToggleOpen={false}
+                                >
+                                    <p>{this.props.exercise.notes}</p>
+                                </ToggledLeftRightListItem>
                             }
                         </List>
                     </CardText>
