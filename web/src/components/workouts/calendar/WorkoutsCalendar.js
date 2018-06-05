13794b85e439fed158902c884e03b431747116a5
JP Dillingham <jpdillingham@gmail.com>
2018-06-04 21:31:27 -0500

    calendar WIP

--- web/src/components/workouts/calendar/WorkoutsCalendar.js
+++ web/src/components/workouts/calendar/WorkoutsCalendar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment';
 
 import { fetchWorkoutsHistory } from '../../workouts/history/WorkoutsHistoryActions';
 import { fetchWorkouts } from '../WorkoutsActions';
@@ -9,6 +10,8 @@ import Spinner from '../../shared/Spinner';
 
 import { red500 } from 'material-ui/styles/colors';
 import ActionHighlightOff from 'material-ui/svg-icons/action/highlight-off';
+import BigCalendar from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 const initialState = {
     filters: {},
@@ -45,6 +48,8 @@ const styles = {
     },
 }
 
+BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment));
+
 class WorkoutsCalendar extends Component {
     constructor(props) {
         super(props);
@@ -61,7 +66,7 @@ class WorkoutsCalendar extends Component {
                 toTime: defaulttoTime.getTime(), 
                 fromTime: defaultfromTime.getTime(),
             } 
-        };        
+        };
     }
 
     componentWillMount() {
@@ -100,16 +105,119 @@ class WorkoutsCalendar extends Component {
     render() {
         let workouts = this.props.workouts.concat(this.props.workoutsHistory.workouts);
 
+        let events = [
+            {
+              id: 0,
+              title: 'All Day Event very long title',
+              allDay: true,
+              start: new Date(2015, 3, 0),
+              end: new Date(2015, 3, 1),
+            },
+            {
+              id: 1,
+              title: 'Long Event',
+              start: new Date(2015, 3, 7),
+              end: new Date(2015, 3, 10),
+            },
+          
+            {
+              id: 2,
+              title: 'DTS STARTS',
+              start: new Date(2016, 2, 13, 0, 0, 0),
+              end: new Date(2016, 2, 20, 0, 0, 0),
+            },
+          
+            {
+              id: 3,
+              title: 'DTS ENDS',
+              start: new Date(2016, 10, 6, 0, 0, 0),
+              end: new Date(2016, 10, 13, 0, 0, 0),
+            },
+          
+            {
+              id: 4,
+              title: 'Some Event',
+              start: new Date(2015, 3, 9, 0, 0, 0),
+              end: new Date(2015, 3, 9, 0, 0, 0),
+            },
+            {
+              id: 5,
+              title: 'Conference',
+              start: new Date(2015, 3, 11),
+              end: new Date(2015, 3, 13),
+              desc: 'Big conference for important people',
+            },
+            {
+              id: 6,
+              title: 'Meeting',
+              start: new Date(2015, 3, 12, 10, 30, 0, 0),
+              end: new Date(2015, 3, 12, 12, 30, 0, 0),
+              desc: 'Pre-meeting meeting, to prepare for the meeting',
+            },
+            {
+              id: 7,
+              title: 'Lunch',
+              start: new Date(2015, 3, 12, 12, 0, 0, 0),
+              end: new Date(2015, 3, 12, 13, 0, 0, 0),
+              desc: 'Power lunch',
+            },
+            {
+              id: 8,
+              title: 'Meeting',
+              start: new Date(2015, 3, 12, 14, 0, 0, 0),
+              end: new Date(2015, 3, 12, 15, 0, 0, 0),
+            },
+            {
+              id: 9,
+              title: 'Happy Hour',
+              start: new Date(2015, 3, 12, 17, 0, 0, 0),
+              end: new Date(2015, 3, 12, 17, 30, 0, 0),
+              desc: 'Most important meal of the day',
+            },
+            {
+              id: 10,
+              title: 'Dinner',
+              start: new Date(2015, 3, 12, 20, 0, 0, 0),
+              end: new Date(2015, 3, 12, 21, 0, 0, 0),
+            },
+            {
+              id: 11,
+              title: 'Birthday Party',
+              start: new Date(2015, 3, 13, 7, 0, 0),
+              end: new Date(2015, 3, 13, 10, 30, 0),
+            },
+            {
+              id: 12,
+              title: 'Late Night Event',
+              start: new Date(2015, 3, 17, 19, 30, 0),
+              end: new Date(2015, 3, 18, 2, 0, 0),
+            },
+            {
+              id: 13,
+              title: 'Multi-day Event',
+              start: new Date(2015, 3, 20, 19, 30, 0),
+              end: new Date(2015, 3, 22, 2, 0, 0),
+            },
+            {
+              id: 14,
+              title: 'Today',
+              start: new Date(new Date().setHours(new Date().getHours() - 3)),
+              end: new Date(new Date().setHours(new Date().getHours() + 3)),
+            },
+          ]
+        
+          let allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k])
+
         return (
             this.state.loadApi.isExecuting ? <Spinner size={48}/> : 
                 this.state.loadApi.isErrored ? <ActionHighlightOff style={{ ...styles.icon, color: red500 }} /> :
-                    <div style={styles.grid}>
-                        <ul>
-                            {workouts.map(w => 
-                                <li key={w.id}>{w.endTime !== undefined ? 'COMPLETE: ' : w.startTime !== undefined ? 'IN PROGRESS: ' : 'SCHEDULED: '}{w.id}</li>
-                            )}
-                        </ul>
-                    </div>
+                    <BigCalendar
+                        events={events}
+                        views={allViews}
+                        step={60}
+                        showMultiDayTimes
+                        defaultDate={new Date(2015, 3, 1)}
+                    />
         )
     }
 }
