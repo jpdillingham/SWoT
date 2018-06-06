692bf0dbe06c43d232054fcfe97a523bf714551e
JP Dillingham <jpdillingham@gmail.com>
2018-06-06 07:35:32 -0500

    add WorkoutsCalendarEvent

---
+++ web/src/components/workouts/calendar/WorkoutsCalendarEvent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+const styles = {
+}
+
+class WorkoutsCalendarEvent extends Component {
+    render() {
+        console.log(this.props)
+        return (
+            <span>Hello World!</span>
+        )
+    }
+}
+
+export default WorkoutsCalendarEvent
