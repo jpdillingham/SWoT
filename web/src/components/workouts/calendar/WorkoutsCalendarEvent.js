3fcd2fda3a1eeac4610a2c3b9947c756ab2d9f36
JP Dillingham <jpdillingham@gmail.com>
2018-06-07 08:26:28 -0500

    add icon style

--- web/src/components/workouts/calendar/WorkoutsCalendarEvent.js
+++ web/src/components/workouts/calendar/WorkoutsCalendarEvent.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 
 import { black, red500, yellow500, green500 } from 'material-ui/styles/colors'
-import { AvPlayArrow } from 'material-ui/svg-icons';
+import { AvPlayArrow, ActionSchedule, AvStop } from 'material-ui/svg-icons';
 
 const styles = {
     container: {
         position: 'relative',
-        borderRadius: 0
+        borderRadius: 0,
+    },
+    icon: {
+        width: 16,
+        height: 16,
     },
     title: {
         position: 'absolute',
-        marginTop: 5,
+        marginTop: 0,
         textOverflow: 'ellipsis',
         color: black,
     }
@@ -19,13 +23,14 @@ class WorkoutsCalendarEvent extends Component {
     render() {
         let event = this.props.event;
         let backgroundColor = event.status === 'scheduled' ? red500 : event.status === 'started' ? yellow500 : green500;
+        let icon = event.status === 'scheduled' ? <ActionSchedule style={styles.icon}/> : 
+            event.status === 'started' ? <AvPlayArrow style={styles.icon}/> : <AvStop style={styles.icon}/>;
 
         return (
             <div>
                 {React.Children.map(this.props.children, child =>
                     <div {...child.props} style={{ ...styles.container, backgroundColor: backgroundColor}}>
-                        <AvPlayArrow/>
-                        <span style={styles.title}>{this.props.event.title}</span>
+                        {icon}<span style={styles.title}>{this.props.event.title}</span>
                     </div>
                 )}
             </div>
