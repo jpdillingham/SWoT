b49d77133b589439480f5f4fd71e4bda25c5381d
JP Dillingham <jpdillingham@gmail.com>
2018-06-13 08:22:32 -0500

    stub line chart example

--- web/src/components/exercises/progress/ExercisesProgress.js
+++ web/src/components/exercises/progress/ExercisesProgress.js
@@ -18,6 +18,8 @@ import { showSnackbar } from '../../app/AppActions';
 
 import { sortByProp } from '../../../util';
 
+import { Line } from 'react-chartjs-2';
+
 const initialState = {
     filters: {
         fromTime: undefined,
@@ -147,6 +149,55 @@ class ExercisesProgress extends Component {
         let metrics = this.getDistinctMetrics(exercises);
         let values = this.getValues(exercises, metrics);
 
+        let chartData = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'My First dataset',
+                    fill: false,
+                    lineTension: 0.1,
+                    backgroundColor: 'rgba(75,192,192,0.4)',
+                    borderColor: 'rgba(75,192,192,1)',
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: 'rgba(75,192,192,1)',
+                    pointBackgroundColor: '#fff',
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                    pointHoverBorderColor: 'rgba(220,220,220,1)',
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                },
+                {
+                    label: 'My First dataset',
+                    fill: false,
+                    lineTension: 0.1,
+                    backgroundColor: 'rgba(75,192,192,0.4)',
+                    borderColor: 'rgba(75,192,192,1)',
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: 'rgba(75,192,192,1)',
+                    pointBackgroundColor: '#fff',
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                    pointHoverBorderColor: 'rgba(220,220,220,1)',
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: [56, 95, 8, 18, 65, 55, 4]
+                }
+            ]
+        };
+
+
         return (
             this.state.loadApi.isExecuting ? <Spinner size={48}/> : 
                 this.state.loadApi.isErrored ? <ActionHighlightOff style={{ ...styles.icon, color: red500 }} /> :
@@ -177,6 +228,7 @@ class ExercisesProgress extends Component {
                                 {JSON.stringify(metrics)}
                                 <br/><br/>
                                 {JSON.stringify(values)}
+                                <Line data={chartData}/>
                                 {this.state.refreshApi.isExecuting ? <Spinner/> : ''}
                             </CardText>
                         </Card>
