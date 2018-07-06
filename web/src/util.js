0988ade6449e9e2c147f914ea4a1c01979ae71bf
JP Dillingham <jpdillingham@gmail.com>
2018-07-05 21:34:34 -0500

    disable linting for regex

--- web/src/util.js
+++ web/src/util.js
@@ -100,12 +100,7 @@ export const hexToRgb = (hexColor) => {
 }
 
 export const validateUrl = (str) => {
-    var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\.)+[a-z]{2,}|'+ // domain name
-      '((\\d{1,3}\.){3}\\d{1,3}))'+ // OR ip (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-      '(\\#[-a-z\\d_]*)?$','i'); // fragment locater
-
+    // eslint-disable-next-line
+    var pattern = new RegExp('^(https?:\/\/)?((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\.)+[a-z]{2,}|((\\d{1,3}\.){3}\\d{1,3}))(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*(\\?[;&a-z\\d%_.~+=-]*)?(\\#[-a-z\d_]*)?$','i');
     return pattern.test(str);
 }
