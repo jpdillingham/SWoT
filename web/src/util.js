3e76397603f4d1605c2b6bf8434b7317fe5dc04d
JP Dillingham <jpdillingham@gmail.com>
2018-07-05 21:31:36 -0500

    escape regex characters

--- web/src/util.js
+++ web/src/util.js
@@ -101,11 +101,11 @@ export const hexToRgb = (hexColor) => {
 
 export const validateUrl = (str) => {
     var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
-      '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
-      '((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
-      '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
-      '(\?[;&a-z\d%_.~+=-]*)?'+ // query string
-      '(\#[-a-z\d_]*)?$','i'); // fragment locater
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\.)+[a-z]{2,}|'+ // domain name
+      '((\\d{1,3}\.){3}\\d{1,3}))'+ // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+      '(\\#[-a-z\\d_]*)?$','i'); // fragment locater
 
     return pattern.test(str);
 }
