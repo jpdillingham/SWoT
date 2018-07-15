5e73c3ec909f5aeada1c4a27095e5ad16ef99eb2
JP Dillingham <jpdillingham@gmail.com>
2018-07-14 21:09:04 -0500

    add reducer for workout history puts

--- web/src/components/workouts/history/WorkoutsHistoryReducer.js
+++ web/src/components/workouts/history/WorkoutsHistoryReducer.js
@@ -8,6 +8,8 @@ const WorkoutsHistoryReducer = (state = initialState, action) => {
             return { workouts: [], totalCount: 0 };
         case 'WORKOUT_HISTORY_GET':
             return { workout: action.workout };
+        case 'WORKOUT_HISTORY_PUT':
+            return { workout: action.workout };
         case 'WORKOUT_HISTORY_CLEAR':
             return { workout: undefined };
         case 'WORKOUT_HISTORY_DELETE':
