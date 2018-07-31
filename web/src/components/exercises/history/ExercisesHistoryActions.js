af6e045d069c662749ce381ffff79a75f5c5aae6
JP Dillingham <jpdillingham@gmail.com>
2018-07-31 08:21:17 -0500

    refactor fetchExercisesHistory to filter exerciseId from query string and add to route

--- web/src/components/exercises/history/ExercisesHistoryActions.js
+++ web/src/components/exercises/history/ExercisesHistoryActions.js
@@ -14,16 +14,19 @@ const exercisesHistoryClear = () => ({
 })
 
 export const fetchExercisesHistory = (filters) => (dispatch, getState) => {
-    let queryParams = '?';
+    let id = '';
 
-    Object.keys(filters).forEach(f => {
-        if (filters[f] !== undefined) {
-            queryParams += '&' + f + '=' + filters[f];
-        }
-    })
+    if (filters.exerciseId) {
+        id = '/' + filters.exerciseId + '/';
+    }
+
+    let queryParams = Object.keys(filters)
+        .filter(f => f !== 'exerciseId')
+        .filter(f => filters[f] !== undefined)
+        .reduce((acc, f) => acc.concat('&' + f + '=' + filters[f]), '?');
 
     return new Promise((resolve, reject) => {
-        api.get(endpoint + queryParams)
+        api.get(endpoint + id + queryParams)
         .then(response => {
             let totalCount = parseInt(response.headers['x-total-count'], 10)
             dispatch(exercisesHistoryGet(response.data, totalCount));
