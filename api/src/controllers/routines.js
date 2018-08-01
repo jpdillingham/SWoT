0212f9e2d6c2fbc97ed1a4a77f06c9be4f8f53fb
JP Dillingham <jpdillingham@gmail.com>
2018-08-01 07:19:49 -0500

    remove deprecated routine history

--- api/src/controllers/routines.js
+++ api/src/controllers/routines.js
@@ -4,52 +4,7 @@ const util = require('../util')
 
 const router = express.Router();
 
-// pagination - /workouts?limit=N&offset=M
-// sort - /workouts?order=<ASC|DESC>
-// filter by routine - /workouts?routineId=guid
-// filter by date range = /workouts?fromDate=<unix timestamp>&toDate=<unix timestamp>
-router.get('/history', (req, res) => {
-    let userId = util.getUserId(req);
-    let order = req.query && req.query.order ? req.query.order.toLowerCase() : undefined;
-    let routineId = req.query && req.query.routineId ? req.query.routineId.toLowerCase() : undefined;
-    let limit = req.query && req.query.limit ? req.query.limit : undefined;
-    let offset = req.query && req.query.offset ? req.query.offset : undefined;
-    let fromTime = req.query && req.query.fromTime ? req.query.fromTime : 0;
-    let toTime = req.query && req.query.toTime ? req.query.toTime : new Date().getTime();
-
-    database.queryAll(userId, fromTime, toTime)
-    .then(workouts => {
-        let routines = workouts
-                        .map(w => w.routine)
-
-        if (routineId) {
-            routines = routines.filter(r => r.id === routineId);
-        }
-
-        res.header('X-Total-Count', routines.length);
-
-        if (order) {
-            if (order === 'asc' || order === 'desc') {
-                routines = routines.sort(util.sortByEndTime(order))
-            }
-            else {
-                res.status(400);
-                res.json('Invalid order predicate \'' + order + '\'; specify ASC or DESC')
-            }
-        }
-
-        if (offset && limit) {
-            routines = routines.slice(+offset, +offset + +limit);
-        }
-
-        res.status(200);
-        res.json(routines);
-    })
-    .catch(err => {
-        res.status(500);
-        res.json(err);
-    })
-})
+router.get('/history', (req, res) => { res.redirect('../workouts/history') });
 
 router.get('/', (req, res) => { 
     let userId = util.getUserId(req);
@@ -65,8 +20,6 @@ router.get('/', (req, res) => {
         res.json(err);
     });
 });
-
-router.get('/history', (req, res) => { res.redirect('../workouts/history') });
 
 router.post('/', (req, res) => {
     // todo: validate input
