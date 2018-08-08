f8db5f68763146e9ff10df5a997d912b8609398a
JP Dillingham <jpdillingham@gmail.com>
2018-08-08 02:57:12 -0500

    add missing semicolons to index.js

--- web/src/index.js
+++ web/src/index.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { combineReducers, createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk'
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { loadState, saveState } from './localStorage';
 
-import App from './components/app/App'
+import App from './components/app/App';
 
-import ExercisesReducer from './components/exercises/ExercisesReducer'
-import ExercisesHistoryReducer from './components/exercises/history/ExercisesHistoryReducer'
+import ExercisesReducer from './components/exercises/ExercisesReducer';
+import ExercisesHistoryReducer from './components/exercises/history/ExercisesHistoryReducer';
 import AppReducer from './components/app/AppReducer';
-import RoutinesReducer from './components/routines/RoutinesReducer'
-import WorkoutsReducer from './components/workouts/WorkoutsReducer'
-import WorkoutsHistoryReducer from './components/workouts/history/WorkoutsHistoryReducer'
-import SecurityReducer from './components/security/SecurityReducer'
+import RoutinesReducer from './components/routines/RoutinesReducer';
+import WorkoutsReducer from './components/workouts/WorkoutsReducer';
+import WorkoutsHistoryReducer from './components/workouts/history/WorkoutsHistoryReducer';
+import SecurityReducer from './components/security/SecurityReducer';
 
 const rootReducer = combineReducers({ 
     app: AppReducer, 
@@ -24,7 +24,7 @@ const rootReducer = combineReducers({
     workouts: WorkoutsReducer,
     workoutsHistory: WorkoutsHistoryReducer,
     security: SecurityReducer,
-})
+});
 
 const persistedState = loadState();
 
@@ -40,8 +40,8 @@ store.subscribe(() => {
         },
     });
 
-    console.log('State Updated:', store.getState())
-})
+    console.log('State Updated:', store.getState());
+});
 
 ReactDOM.render(
     <Provider store={store}>
