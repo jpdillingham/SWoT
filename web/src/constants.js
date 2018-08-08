d0366b57aa08ef2ccdf2ed7a67d39f09ebc9e6da
JP Dillingham <jpdillingham@gmail.com>
2018-08-08 02:59:58 -0500

    add missing semicolons to api.js, constants.js, localStorage.js

--- web/src/constants.js
+++ web/src/constants.js
@@ -5,24 +5,24 @@ import {
     deepPurple500, lightBlue500, lightGreen500, amber500,
 } from "material-ui/styles/colors";
 
-export const API_ROOT = "https://u8sie6ns6c.execute-api.us-east-1.amazonaws.com/deployment"
+export const API_ROOT = "https://u8sie6ns6c.execute-api.us-east-1.amazonaws.com/deployment";
 
-export const EXERCISE_TYPES = [ 'Weightlifting', 'Bodyweight', 'Cardio', 'Stretching', 'Balance' ]
+export const EXERCISE_TYPES = [ 'Weightlifting', 'Bodyweight', 'Cardio', 'Stretching', 'Balance' ];
 
-export const WORKOUT_AVATAR_COLOR = '#f44336'
-export const EXERCISE_AVATAR_COLOR = '#00c853'
+export const WORKOUT_AVATAR_COLOR = '#f44336';
+export const EXERCISE_AVATAR_COLOR = '#00c853';
 
-export const ROUTINE_AVATAR_COLOR = '#ffab00'
-export const CARD_WIDTH = 390
+export const ROUTINE_AVATAR_COLOR = '#ffab00';
+export const CARD_WIDTH = 390;
 
 export const INTENTS = {
     ADD: 'ADD',
     EDIT: 'EDIT',
     COPY: 'COPY',
-}
+};
 
-export const COGNITO_POOLID = 'us-east-1_OhnX3yEY5'
-export const COGNITO_CLIENTID = '18b132av2gkfgl3m793qcgjsd3'
+export const COGNITO_POOLID = 'us-east-1_OhnX3yEY5';
+export const COGNITO_CLIENTID = '18b132av2gkfgl3m793qcgjsd3';
 
 export const FILTER_LIMIT_OPTIONS = [ 
     { caption: '5', value: 5 }, 
